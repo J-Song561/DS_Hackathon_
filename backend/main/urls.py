10e615b3ab23b243d7a14f5e506cbdc53c45102b
HJ <annesong03@gmail.com>
2025-11-22 16:00:30 +0900

    urls수정

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 from django.contrib import admin
 from .views import VideoHazardAnalyzeView
-from .thumbnail_classifier import ThumbnailEncoder
 
 urlpatterns = [ 
-    path("admin/",admin.site.urls),
-    path("analyze/transctipt/",VideoHazardAnalyzeView.as_view(), name = "transcript-analyzer"),
-    path("analyze/thumbnail/",ThumbnailEncoder.as_view(), name="thumbnail-analyzer")
+    path("admin/", admin.site.urls),
+    path("api/analyze/", VideoHazardAnalyzeView.as_view(), name="video-analyzer"),
 ]
-
